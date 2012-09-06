26d24b19231f93b30c1c29818b928a21963ca91a
Scott Meitz <mirage123@frontier.com>
2012-09-06 12:46:02 -0400

    Fix always blocking ID0 for protection for now. Viewer is blocked (IO) was triggering good or bad forcing the protection to be set to false just so users can log in. This allows at least part of that ban system to work.

--- Aurora/Modules/World/BannedViewersModule/BanCheck.cs
+++ Aurora/Modules/World/BannedViewersModule/BanCheck.cs
@@ -461,13 +461,15 @@ namespace Aurora.Modules.Ban
                     return false;
                 }
             }
-            //foreach (string id0 in info.KnownID0s)
+            foreach (string id0 in info.KnownID0s) //was commented out the below is active -VS
             {
-                if (info.LastKnownID0.Contains("000"))
+                //if (info.LastKnownID0.Contains("000"))
                 {
                     //Ban this asshole
-                    reason = "Viewer is blocked (IO)";
-                    return false;
+                    //reason = "Viewer is blocked (IO)";
+                    //return false;
+                    reason = "Ok ";//just say ok for now
+                    return true;//let them in for now till this is fixed
                 }
             }
 
